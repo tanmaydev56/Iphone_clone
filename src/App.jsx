be3398ce5b4db3c5f5ed7239a0a8fb3e6c782498
tanmaydev56 <tanmaysharma763@gmail.com>
2024-06-10 12:09:41 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import Features from './components/Features'
+import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import Model from './components/Model'
@@ -17,6 +19,8 @@ const App=()=> {
    <Hero/>
    <Highlights/>
   <Model/>
+  <Features/>
+  <Footer/>
     </main>
     </>
   )
